7fb9e5aa45cac68d9e971e390c36d7ec91ad8182
Joel Matthews <joel@joelmatth.com>
2018-12-11 22:53:22 +0000

    Use Lombok @Data with Gig

--- src/main/java/com/joelmatth/gigstream/Gig.java
+++ src/main/java/com/joelmatth/gigstream/Gig.java
@@ -3,10 +3,12 @@ package com.joelmatth.gigstream;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import lombok.Data;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDate;
 
+@Data
 @Document
 public class Gig {
 
@@ -19,48 +21,8 @@ public class Gig {
     private String location;
     private String name;
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getArtist() {
-        return artist;
-    }
-
-    public void setArtist(String artist) {
-        this.artist = artist;
-    }
-
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDate date) {
-        this.date = date;
-    }
-
     public int getYear() {
         return date.getYear();
-    }
-
-    public String getLocation(){
-        return location;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
 }
