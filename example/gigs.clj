7e62d33257c1ca81013e91f2855cf602ef7d6e8f
Joel Matthews <joel@joelmatth.com>
2020-09-17 20:21:00 +0100

    Remove example gigs index in Clojure

--- example/gigs.clj
+++
@@ -1,4 +0,0 @@
-[{:artist "Radiohead" :date "2017-06-23" :location "Glastonbury"}
- {:artist "Primal Scream" :date "2011-07-22" :location "Auditorium Parco Della Musica"}
- {:artist "Ben Howard" :date "2014-12-17" :location "Vorst Nationaal"}
- {:artist "LCD Soundsystem" :date "2010-10-27" :location "The Fillmore Detroit"}]
