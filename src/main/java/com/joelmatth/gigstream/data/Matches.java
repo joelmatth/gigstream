650e8966cab6e9392e1bc43cebf87f846d18fafa
Joel Matthews <joel@joelmatth.com>
2021-01-20 15:48:25 +0000

    Reduce visibility of Matches class and methods

--- src/main/java/com/joelmatth/gigstream/data/Matches.java
+++ src/main/java/com/joelmatth/gigstream/data/Matches.java
@@ -2,25 +2,25 @@ package com.joelmatth.gigstream.data;
 
 import com.joelmatth.gigstream.Gig;
 
-public class Matches {
+class Matches {
 
-    public static boolean artist(Gig gig, String term) {
+    static boolean artist(Gig gig, String term) {
         return termMatch(gig, Gig::getArtist, term);
     }
 
-    public static boolean date(Gig gig, String term) {
+    static boolean date(Gig gig, String term) {
         return termMatch(gig, g -> g.getDate().toString(), term);
     }
 
-    public static boolean location(Gig gig, String term) {
+    static boolean location(Gig gig, String term) {
         return termMatch(gig, Gig::getLocation, term);
     }
 
-    public static boolean name(Gig gig, String term) {
+    static boolean name(Gig gig, String term) {
         return termMatch(gig, Gig::getName, term);
     }
 
-    public static boolean termMatch(Gig gig, Getter getter, String term) {
+    static boolean termMatch(Gig gig, Getter getter, String term) {
         String value = getter.get(gig);
         if (value == null) return false;
         return value.toLowerCase().contains(term.toLowerCase());
